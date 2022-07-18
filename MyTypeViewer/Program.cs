b109925b7bd92a4975175e8af77f0631c4a830bc
Sergey Misko <sergik.m@outlook.com>
2022-07-18 22:58:49 +0500

    рефакторил ListMethods() и ListFields()

--- MyTypeViewer/Program.cs
+++ MyTypeViewer/Program.cs
@@ -28,9 +28,7 @@ namespace MyTypeViewer
             Console.WriteLine("***** Methods *****");
             // MethodInfo[] mi = t.GetMethods();
             var methodNames = from n in t.GetMethods() select n.Name;
-            foreach(var name in methodNames)
-                Console.WriteLine("-> {0}",name);
-            Console.WriteLine();
+            PrintCollection(methodNames);
         }
 
         // Отобразить имена полей в типе.
@@ -38,9 +36,7 @@ namespace MyTypeViewer
         {
             Console.WriteLine("***** Fields *****");
             var fieldNames = from f in t.GetFields() select f.Name;
-            foreach (var name in fieldNames)
-                Console.WriteLine("-> {0}", name);
-            Console.WriteLine();
+            PrintCollection(fieldNames);
         }
     }
 }
