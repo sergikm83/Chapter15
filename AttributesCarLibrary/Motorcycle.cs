6ab0252af2404e68f738ebdcc5fa5d5b7d02f3b3
Sergey Misko <spy_3462@mail.ru>
2022-07-19 17:32:07 +0400

    добавил класс Motorcycle

---
+++ AttributesCarLibrary/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesCarLibrary
+{
+    // Назначить описание с помощью "именованного свойства".
+    [Serializable]
+    [VehicleDescription(Description = "My rocking Harley")]
+    public class Motorcycle
+    {
+
+    }
+}
