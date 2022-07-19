e636b9fa8ea61436ec442c0546fafb5c0c4b2814
Sergey Misko <spy_3462@mail.ru>
2022-07-19 17:35:50 +0400

    добавил класс Winnebago

---
+++ AttributesCarLibrary/Winnebago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesCarLibrary
+{
+    // Назначить описание с помощью "именованного свойства".
+    [VehicleDescription("A very long, slow, but feature-rich auto")]
+    public class Winnebago
+    {
+
+    }
+}
