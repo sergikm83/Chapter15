dbe422eefab4371dae2bb3129eebfa03173517f8
Sergey Misko <spy_3462@mail.ru>
2022-07-19 13:31:50 +0400

    подклчил необходимые пространства имен для работы

--- ExternalAssemblyReflector/Program.cs
+++ ExternalAssemblyReflector/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
 
 namespace ExternalAssemblyReflector
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
