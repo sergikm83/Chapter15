8a2350c1d495224061f20fd69446004aeb12d067
Sergey Misko <spy_3462@mail.ru>
2022-07-19 16:34:18 +0400

    добавил класс Motorcycle и пометил его как сериализуемый

---
+++ ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    [Serializable]
+    public class Motorcycle
+    {
+        // Однако это поле сохраняться не будет.
+        [NonSerialized]
+        float weightOfCurrentPessengers;
+        // Эти поля останутся сериализуемыми.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+}
