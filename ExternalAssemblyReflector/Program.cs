e69fd926f608474b390f54c67513f3dd73ee8ffe
Sergey Misko <spy_3462@mail.ru>
2022-07-19 13:59:19 +0400

    добавил метод DisplayTypesInAsm() для вывода информации о содержимом сборки (классы, методы, интерфейсы, структуры и т.д.). Также наполнил Main основным кодом

--- ExternalAssemblyReflector/Program.cs
+++ ExternalAssemblyReflector/Program.cs
@@ -9,10 +9,39 @@ namespace ExternalAssemblyReflector
 {
     class Program
     {
-
-        static void Main(string[] args)
+        static void DisplayTypesInAsm(Assembly asm)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n***** Types in Assembly *****");
+            Console.WriteLine("->{0}", asm.FullName);
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types)
+                Console.WriteLine("Type: {0}", t);
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            Console.WriteLine("***** External Assembly Viewer *****");
+            string asmName = "";
+            Assembly asm = null;
+            do
+            {
+                // Запрос приглашение на ввод имени сборки
+                // либо выход по вводу Q
+                Console.WriteLine("\nEnter an assembly to evaluate");
+                Console.Write("or enter Q to quit: ");
+                // Получить имя сборки.
+                asmName = Console.ReadLine();
+                if (asmName.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                    break;
+                // Попробовать загрузить сборку.
+                try
+                {
+                    asm = Assembly.Load(asmName);
+                    DisplayTypesInAsm(asm);
+                }
+                catch
+                { Console.WriteLine("Sorry, can't find assembly."); }
+            } while (true);
         }
     }
 }
