a96190c041b683721e9448840f39b64f14b98120
Sergey Misko <spy_3462@mail.ru>
2022-07-19 14:28:45 +0400

    добавил новый проект LateBindingApp

---
+++ LateBindingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace LateBindingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
