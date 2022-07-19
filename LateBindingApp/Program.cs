e1bc5f952a676112fc8421045f9483e58bde3a04
Sergey Misko <spy_3462@mail.ru>
2022-07-19 14:32:59 +0400

    добавил основной код в Main для загрузки внешней сборки CarLibrary

--- LateBindingApp/Program.cs
+++ LateBindingApp/Program.cs
@@ -4,11 +4,28 @@ using System.Reflection;
 
 namespace LateBindingApp
 {
+    // Это приложение будет загружать внешнюю сборку и
+    // создавать объект, используя позднее связывание.
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Late Binding *****");
+            // Попробовать загрузить локальную копию CarLibrary.
+            Assembly a = null;
+            try
+            {
+                a = Assembly.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (a != null)
+                // continue this
+                ;
         }
+
     }
 }
