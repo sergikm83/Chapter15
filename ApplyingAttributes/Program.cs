b383cd7e85457d3b7fc7341f8785b7d9bebda7cc
Sergey Misko <spy_3462@mail.ru>
2022-07-19 16:47:45 +0400

    создал экземпляр HorseAndBuggy и посмотрел подсказку от VS о том, что данный класс устаревший, т.к. помечен атрибутом Obsolete

--- ApplyingAttributes/Program.cs
+++ ApplyingAttributes/Program.cs
@@ -6,7 +6,7 @@ namespace ApplyingAttributes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HorseAndBuggy mule = new HorseAndBuggy();
         }
     }
 }
