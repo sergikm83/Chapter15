ffe895111d72d9c7adaf3daa151ccf15d36689c9
Sergey Misko <sergik.m@outlook.com>
2022-07-20 22:30:52 +0500

    добавил новый проект VehicleDescriptionAttributeReaderLateBinding

---
+++ VehicleDescriptionAttributeReaderLateBinding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleDescriptionAttributeReaderLateBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
