9cc0f56492d1add45f9462ed771c523bb42e0936
Sergey Misko <spy_3462@mail.ru>
2022-07-20 16:44:59 +0400

    с помощью атрибута AttributeUsage изменим область применения атрибута VehicleDescriptionAttribute к классам и структурам

--- AttributesCarLibrary/VehicleDescriptionAttribute.cs
+++ AttributesCarLibrary/VehicleDescriptionAttribute.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace AttributesCarLibrary
 {
+    // На этот раз для аннотирования специального атрибута
+    // мы используем атрибут AttributeUsage.
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
     public sealed class VehicleDescriptionAttribute : System.Attribute
     {
         public string Description { get; set; }
