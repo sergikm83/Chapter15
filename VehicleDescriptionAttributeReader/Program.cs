25a08fe253415a60ebcada226ffd81c40ac3d39d
Sergey Misko <sergik.m@outlook.com>
2022-07-20 22:12:08 +0500

    добавил новый проект VehicleDescriptionAttributeReader

---
+++ VehicleDescriptionAttributeReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleDescriptionAttributeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
